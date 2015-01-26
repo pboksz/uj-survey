1758e5f2cac8b547298aa3d95b27e9bd2f297027
Phillip Boksz <phillip@lunarlogic.io>
2015-01-26 11:52:01 +0100

    Overwrite as_json method to have questions and answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answers, dependent: :destroy
+
+  def as_json(options = {})
+    super(options).merge(answers: answers.map(&:as_json))
+  end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,7 @@
 class Survey < ActiveRecord::Base
   has_many :questions, dependent: :destroy
+
+  def as_json(options = {})
+    super(options).merge(questions: questions.map(&:as_json))
+  end
 end
