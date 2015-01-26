013d0405cda4ae583f27b03091e13440076314dd
Phillip Boksz <phillip@lunarlogic.io>
2015-01-26 18:05:13 +0100

    Add survey response association

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,3 @@
 class Response < ActiveRecord::Base
-
+  belongs_to :survey
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ActiveRecord::Base
   has_many :questions, dependent: :destroy
+  has_many :responses, dependent: :destroy
 
   def as_json(options = {})
     super(options).merge(questions: questions.map(&:as_json))
