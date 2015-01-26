1a689db306932038ed64a05eac88a21742774070
Phillip Boksz <phillip@lunarlogic.io>
2015-01-26 17:26:31 +0100

    Make question kind a dropdown

--- app/assets/javascripts/services/QuestionService.coffee
+++ app/assets/javascripts/services/QuestionService.coffee
@@ -2,7 +2,7 @@ angular.module('ujSurvey').factory 'Question', [
   'Answer', (Answer) ->
     new: (order = 0) ->
       order: order
-      kind: ''
+      kind: 'radio_button'
       text: ''
       answers: [Answer.new()]
 ]
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,10 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answers, dependent: :destroy
 
+  def self.kind_options
+    { radio_button: 'Single response', check_box: 'Multiple response', text_area: 'Text response' }
+  end
+
   def as_json(options = {})
     super(options).merge(answers: answers.map(&:as_json))
   end
