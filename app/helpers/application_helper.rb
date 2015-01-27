a0449211a9eceac16ae56472cc616c17ae7dbc4f
Phillip Boksz <phillip@lunarlogic.io>
2015-01-27 14:00:44 +0100

    Simplify answer_tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,14 @@
 module ApplicationHelper
   def answer_tag(question, answer)
-    case question.kind
-      when 'radio_button'
-        radio_button(question, answer)
-      when 'check_box'
-        check_box(question, answer)
-      else
-        answer.text
+    kind_tag = "#{question.kind}_tag"
+
+    if respond_to?(kind_tag) # radio_button or check_box
+      name = "survey[responses][#{question.id}][]"
+      id = "survey_responses_#{question.id}_#{answer.id}"
+
+      send(kind_tag, name, answer.id, false, id: id) + label_tag(id, answer.text)
+    else
+      answer.text
     end
-  end
-
-  private
-
-  def radio_button(question, answer)
-    name = "survey[responses][#{question.id}]"
-    id = "survey_responses_#{question.id}_#{answer.id}"
-
-    radio_button_tag(name, answer.id, false, id: id) + label_tag(id, answer.text)
-  end
-
-  def check_box(question, answer)
-    name = "survey[responses][#{question.id}][]"
-    id = "survey_responses_#{question.id}_#{answer.id}"
-
-    check_box_tag(name, answer.id, false, id: id) + label_tag(id, answer.text)
   end
 end
