1b294add53e03fb45a71e6ca8ca899b5cbda4dd1
Phillip Boksz <phillip@lunarlogic.io>
2015-01-27 12:17:44 +0100

    Only support Single and Multiple responses

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,6 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
   def self.kind_options
-    { radio_button: 'Single response', check_box: 'Multiple response', text_area: 'Text response' }
-  end
-
-  def as_json(options = {})
-    super(options).merge(answers: answers.map(&:as_json))
+    { radio_button: 'Single response', check_box: 'Multiple response' }
   end
 end
