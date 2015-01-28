fd396156b071259577e7ad86cba1526251150a38
Phillip Boksz <phillip@lunarlogic.io>
2015-01-28 15:33:09 +0100

    Add answer order and text helper to response model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answers, dependent: :destroy
+  has_many :responses, dependent: :destroy
 
   validates :survey, :order, :kind, :text, presence: true
 
--- app/models/response.rb
+++ app/models/response.rb
@@ -5,4 +5,22 @@ class Response < ActiveRecord::Base
   serialize :text, Array
 
   validates :survey, :question, :text, presence: true
+
+  def answers_order
+    answers.map { |a| a.order + 1 }.join(', ')
+  end
+
+  def answers_text
+    answers.map { |a| "#{a.order + 1}: #{a.text}" }.join(', ')
+  end
+
+  private
+
+  def answers
+    @answers ||= answers_repository.find_all(text)
+  end
+
+  def answers_repository
+    @answers_repository ||= DefaultRepository.new(question.answers)
+  end
 end
